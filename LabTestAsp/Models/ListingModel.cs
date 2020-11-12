8bea75ba776d8da2df8a67e73a863b6f4052b98e
Jan Mistarz <jan.mistarz6767@gmail.com>
2020-11-12 22:36:51 +0100

    lab 2

---
+++ LabTestAsp/Models/ListingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LabTestAsp.Models
+{ 
+   public class ListingModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+    }
+ }
+
---
+++ LabTestAsp/wwwroot/js/addNewListing.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    const alertElement = document.getElementById("success-alert");
+    const formElement = document.forms[0];
+    const addNewItem = async () => {
+        let formData = {
+            Name: "aThing",
+            Description: "FancyThing Here ",
+            IsVisible: true
+        };
+
+        const response = await fetch("/api/Listing", {
+            method: 'POST',
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(formData)
+        })
+
+        if (response.ok) {
+            alertElement.style.display = "block";
+        }
+    };
+    window.addEventListener("load", () => {
+        formElement.addEventListener("submit", event => {
+            event.preventDefault();
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+})();
